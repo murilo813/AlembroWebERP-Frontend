f5d2171d6a1da8958f8ac81d884ef10d9edbaac9
murilo813 <murilodesouza555@gmail.com>
2026-03-03 10:45:17 -0300

    feat: toast generalizado para erro e sucesso

--- src/components/common/Toast.vue
+++ src/components/common/Toast.vue
@@ -2,7 +2,7 @@
   <Transition name="toast-fade">
     <div v-if="show" class="toast-container" :class="type">
       <div class="toast-content">
-        <i class="fa-solid fa-circle-exclamation toast-icon"></i>
+        <i :class="['fa-solid', iconClass, 'toast-icon']"></i>
         <span class="toast-message">{{ message }}</span>
       </div>
       <div class="toast-progress"></div>
@@ -11,10 +11,16 @@
 </template>
 
 <script setup>
-defineProps({
+import { computed } from 'vue';
+
+const props = defineProps({
   show: Boolean,
   message: String,
   type: { type: String, default: 'error' }
+});
+
+const iconClass = computed(() => {
+  return props.type === 'success' ? 'fa-circle-check' : 'fa-circle-exclamation';
 });
 </script>
 
