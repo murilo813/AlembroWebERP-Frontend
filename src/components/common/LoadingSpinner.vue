63b34670ef05da231e48ce329229790be184ea0d
murilo813 <murilodesouza555@gmail.com>
2026-03-05 14:06:17 -0300

    feat: loading agora é um componente aparte e reutilizável

---
+++ src/components/common/LoadingSpinner.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="empty-search-state">
+    <i class="fa-solid fa-circle-notch fa-spin empty-icon" style="color: #10b981; opacity: 1;"></i>
+    <h2 style="color: #10b981;">{{ title }}</h2>
+    <p>{{ subtitle }}</p>
+  </div>
+</template>
+
+<script setup>
+import { defineProps } from 'vue';
+
+const props = defineProps({
+  title: {
+    type: String,
+    default: 'Carregando dados...'
+  },
+  subtitle: {
+    type: String,
+    default: 'Sincronizando com o banco de dados'
+  }
+});
+</script>
+
+<style src="./LoadingSpinner.css" scoped></style>
